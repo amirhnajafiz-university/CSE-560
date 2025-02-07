43da4075d1d98a2125ec1411de07b2ed13c86cfe
amirhnajafiz <najafizadeh21@gmail.com>
2025-02-07 17:00:09 -0500

    feat: flask index

--- app/static/main.js
+++ app/static/main.js
@@ -1,13 +1,17 @@
 /* basic D3.js example */
 var data = [4, 8, 15, 16, 23, 42];
 
-var x = d3.scale.linear()
+var x = d3.scaleLinear()
     .domain([0, d3.max(data)])
     .range([0, 420]);
 
-d3.select(".chart")
+d3.select("#chart")
     .selectAll("div")
     .data(data)
     .enter().append("div")
     .style("width", function(d) { return x(d) + "px"; })
+    .style("background-color", "steelblue")
+    .style("color", "white")
+    .style("padding", "5px")
+    .style("margin", "1px")
     .text(function(d) { return d; });
