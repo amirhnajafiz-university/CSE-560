f994a7dab2c1b86b07b102ebcbd8373ec599cf11
amirhnajafiz <najafizadeh21@gmail.com>
2025-02-10 13:07:14 -0500

    remove: boolean data

--- setup.py
+++ setup.py
@@ -3,17 +3,29 @@ import pandas as pd
 
 
 
+# global variables
 DATASET = "data/laptop_prices.csv"
 EXPORT = "data/500_laptop_prices.csv"
 
+titles = [
+    "Company", "Model", "Laptop", "Screen Size", "RAM", "OS", "Weight", "Price_euros",
+    "Screen", "Screen Width", "Screen Height", "Touchscreen", "IPSPanel", "Retina Display",
+    "CPU Company", "CPU Frequency", "CPU Model", "Primary Storage", "Secondary Storage",
+    "Primary Storage Type", "Secondary Storage Type", "GPU Company", "GPU Model"
+]
+
 # reading dataset
 print(f"reading {DATASET} ...")
-df = pd.read_csv(DATASET)
+df = pd.read_csv(DATASET, header=0, names=titles)
 
 # drop none data
 print("dropping none ...")
 df.replace('', np.nan, inplace=True)
 df.dropna(inplace=True)
+
+# drop boolean data
+print("dropping boolean data ...")
+df.drop(columns=['Touchscreen', 'IPSPanel', 'Retina Display', 'Secondary Storage Type'], inplace=True)
 
 # select 500 items
 print("sampling ...")
@@ -21,12 +33,8 @@ df = df.sample(n=500)
 
 # change 'price euro' to 'price' and convert the values to dollars
 print("converting price euro to price in dollars ...")
-df['price'] = df['Price_euros'].apply(lambda x: round(x * 1.19, 2))
+df['Price'] = df['Price_euros'].apply(lambda x: round(x * 1.19, 2))
 df.drop(columns=['Price_euros'], inplace=True)
-
-# process column titles
-print("processing column titles ...")
-df.columns = df.columns.str.replace('_', ' ').str.replace(r'(?<!^)(?=[A-Z])', ' ').str.lower()
 
 # export to new dataset file
 print(f"exporting {EXPORT} ...")
