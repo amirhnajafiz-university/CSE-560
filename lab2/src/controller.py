ec754838f5ab632a88afd24dde34a42e055c0316
amirhnajafiz <najafizadeh21@gmail.com>
2025-02-26 13:48:09 -0500

    add: lab2 base files

--- lab2/src/controller.py
+++ lab2/src/controller.py
@@ -1,15 +1,19 @@
+import json
+import pandas as pd
+from flask import jsonify
+
+
+
 def data_column(column_name):
     """
     Return the data from the csv file for a specific column.
     :param column_name: The name of the column to return.
     :return: The data from the csv file for the specified column.
     """
-    import pandas as pd
     df = pd.read_csv('./data/500_laptop_prices.csv')
     if column_name in df.columns:
         return df[column_name].to_json(orient='records')
     else:
-        from flask import jsonify
         return jsonify({"error": "Column not found"}), 404
 
 def data_column_type(column_name):
@@ -18,17 +22,13 @@ def data_column_type(column_name):
     :param column_name: The name of the column to check.
     :return: The type of data for the specified column.
     """
-    import json
     with open('./data/metadata.json') as f:
         metadata = json.load(f)
     if column_name in metadata["categorical"]:
-        from flask import jsonify
         return jsonify({"type": "categorical"})
     elif column_name in metadata["numerical"]:
-        from flask import jsonify
         return jsonify({"type": "numerical"})
     else:
-        from flask import jsonify
         return jsonify({"error": "Column not found"}), 404
 
 def data_column_mapping(column_name):
@@ -37,14 +37,11 @@ def data_column_mapping(column_name):
     :param column_name: The name of the column to return the mapping for.
     :return: The mapping list for the specified column.
     """
-    import json
     with open('./data/mappings.json') as f:
         mapping = json.load(f)
     if column_name in mapping:
-        from flask import jsonify
         return jsonify(mapping[column_name])
     else:
-        from flask import jsonify
         return jsonify({"error": "Column not found"}), 404
 
 def data():
@@ -52,7 +49,6 @@ def data():
     Return the data from the csv file.
     :return: The data from the csv file.
     """
-    import pandas as pd
     df = pd.read_csv('./data/500_laptop_prices.csv')
     return df.to_json(orient='records')
 
@@ -61,7 +57,5 @@ def headers():
     Return the data headers from the csv file.
     :return: The data headers from the csv file.
     """
-    import pandas as pd
     df = pd.read_csv('./data/500_laptop_prices.csv')
-    from flask import jsonify
     return jsonify(df.columns.tolist())
--- lab2/src/views.py
+++ lab2/src/views.py
@@ -1,7 +1,10 @@
+from flask import render_template
+
+
+
 def home():
     """
     Render the index.html file.
     :return: The rendered index.html file.
     """
-    from flask import render_template
     return render_template('index.html')
