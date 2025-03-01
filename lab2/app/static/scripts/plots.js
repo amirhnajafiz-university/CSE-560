79b3a1472715e7df020adf57e433e8e870f26b9c
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-01 06:31:21 -0500

    fix: bugs in style

--- lab2/app/static/scripts/plots.js
+++ lab2/app/static/scripts/plots.js
@@ -4,8 +4,31 @@ const height = 600;
 const margin = { top: 40, right: 40, bottom: 60, left: 60 };
 
 // Set up global variables
+let scaleLevel = 0.5;
 let selectedPCAs = [];
 let selectedComponents = [];
+
+let currentPCAs = [];
+let loadings = null;
+let principalComponents = null;
+
+// --- Helper Functions ---
+/**
+ * Increase the scale level of the PCA biplot.
+ */
+function incScale() {
+  scaleLevel += 0.5;
+  plotPCA(currentPCAs);
+}
+
+/**
+ * Decrease the scale level of the PCA biplot.
+ */
+function decScale() {
+  if (scaleLevel <= 0.5) return;
+  scaleLevel -= 0.5;
+  plotPCA(currentPCAs);
+}
 
 /**
  * Return the SVG element by Id.
@@ -15,7 +38,7 @@ let selectedComponents = [];
 function getSVG(id) {
   const svg = d3.select(id);
   svg.selectAll("*").remove();
-  svg.attr("width", width).attr("height", height);
+  svg.attr("width", width).attr("height", height).style("overflow", "hidden");
 
   return svg
 }
@@ -42,6 +65,39 @@ function addToSelectedComponents(component) {
   }
 }
 
+/**
+ * Fetch the loadings and principal components for the selected components.
+ * @param {Array} components
+ */
+async function fetchLoadings(components) {
+  loadings = await d3.json(`/api/loadings?components=${components.join(",")}`);
+  principalComponents = await d3.json(`/api/principalcomponents?components=${components.join(",")}`);
+}
+
+/**
+ * Get the loadings and principal components for the selected components.
+ */
+async function getComponents() {
+  let components = [];
+  if (selectedComponents.length >= 2) {
+    components = selectedComponents.sort((a, b) => a - b).slice(0, 2);
+  } else if (selectedComponents.length === 1) {
+    components = [1, selectedComponents[0]];
+  } else {
+    components = [1, 2];
+  }
+
+  // convert them to PC{i}
+  components = components.map(c => `PC${c}`);
+  currentPCAs = components;
+
+  // fetch the loadings and principal components
+  await fetchLoadings(components);
+
+  return components;
+}
+
+// --- Plot Functions ---
 /**
  * Plots the eigenvalues of our sampled dataset.
  */
@@ -181,6 +237,7 @@ async function plotEigenvalues() {
       if (selectedPCAs.includes(i)) {
         selectedPCAs = selectedPCAs.filter(index => index !== i);
       } else {
+        if (selectedPCAs.length == 2) { return; }
         selectedPCAs.push(i);
       }
 
@@ -211,7 +268,7 @@ async function plotEigenvalues() {
       d3.selectAll(".data-label").attr("font-size", (d, _) => (selectedPCAs.includes(d) ? "10px" : "6px"));
 
       // call plotPCA
-      plotPCA();
+      getComponents().then((c) => plotPCA(c));
     }
 
     // call onSelectedAction with the elbow index
@@ -224,178 +281,170 @@ async function plotEigenvalues() {
 
 /**
  * Plots the PCA biplot of our sampled dataset.
+ * @param {Array} components
  */
-async function plotPCA() {
-  let components = [];
-  if (selectedComponents.length >= 2) {
-    components = selectedComponents.sort((a, b) => a - b).slice(0, 2);
-  } else if (selectedComponents.length === 1) {
-    components = [1, selectedComponents[0]];
-  } else {
-    components = [1, 2];
-  }
+async function plotPCA(components) {
+  const eigenvectors = loadings.loadings;
+  const dataPoints = principalComponents.principal_components;
 
-  // convert them to PC{i}
-  components = components.map(c => `PC${c}`);
-
-  Promise.all([
-    d3.json(`/api/loadings?components=${components.join(",")}`),
-    d3.json(`/api/principalcomponents?components=${components.join(",")}`)
-  ]).then(([ev, dp]) => {
-    const eigenvectors = ev.loadings;
-    const dataPoints = dp.principal_components;
-
-    // get the SVG element and set its dimensions
-    const svg = getSVG("#biplot");
-
-    // calculate the domain based on the data points
-    const xExtent = d3.extent(dataPoints, d => d[1]);
-    const yExtent = d3.extent(dataPoints, d => d[2]);
-
-    // ensure the center is at (0, 0)
-    const xDomain = [Math.min(xExtent[0], -xExtent[1]), Math.max(xExtent[1], -xExtent[0])];
-    const yDomain = [Math.min(yExtent[0], -yExtent[1]), Math.max(yExtent[1], -yExtent[0])];
-
-    const xScale = d3.scaleLinear().domain(xDomain).range([margin.left, width - margin.right]).nice();
-    const yScale = d3.scaleLinear().domain(yDomain).range([height - margin.bottom, margin.top]).nice();
-
-    // axes
-    svg.append("g")
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(xScale));
-    svg.append("g")
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(yScale));
-
-    // add grid lines
-    svg.append("g")
-      .attr("class", "grid")
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(yScale)
-        .tickSize(-width + margin.left + margin.right)
-        .tickFormat("")
-        .ticks(20)
-      );
-    
-    svg.append("g")
-      .attr("class", "grid")
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(xScale)
-        .tickSize(-height + margin.top + margin.bottom)
-        .tickFormat("")
-        .ticks(20)
-      );
-
-    // add title
-    svg.append("text")
-      .attr("x", (width / 2))
-      .attr("y", margin.top / 2)
-      .attr("text-anchor", "middle")
-      .attr("class", "title")
-      .text("PCA Biplot of the Sampled Dataset");
-
-    // add axis labels
-    svg.append("text")
-      .attr("x", width / 2)
-      .attr("y", height - margin.bottom / 4)
-      .attr("text-anchor", "middle")
-      .attr("class", "axis-label")
-      .text(components[0]);
-    
-    svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("x", -height / 2)
-      .attr("y", margin.left / 4)
-      .attr("text-anchor", "middle")
-      .attr("class", "axis-label")
-      .text(components[1]);
-
-    // plot data points
-    svg.selectAll(".pca-point")
-      .data(dataPoints)
-      .enter()
-      .append("circle")
-      .attr("class", "pca-point")
-      .attr("cx", d => xScale(d[1]))
-      .attr("cy", d => yScale(d[2]))
-      .attr("r", 4)
-      .style("fill", "steelblue")
-      .style("opacity", 0.6)
-      .on("mouseover", function(_, d) {
-        // append a group to hold the tooltip elements
-        let tooltip = svg.append("g")
-          .attr("class", "popup");
+  // get the SVG element and set its dimensions
+  const tmp = getSVG("#biplot");
+  tmp.append("defs")
+    .append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("x", margin.left)
+    .attr("y", margin.top)
+    .attr("width", width - margin.left - margin.right)
+    .attr("height", height - margin.top - margin.bottom);
   
-        // append a rectangle for background
-        tooltip.append("rect")
-          .attr("x", xScale(d[1]) - 15)
-          .attr("y", yScale(d[2]) - 15)
-          .attr("width", 60)
-          .attr("height", 15)
-          .attr("fill", "#222")
-          .attr("stroke", "#ccc")
-          .attr("rx", 5) // rounded corners
-          .attr("ry", 5)
-          .style("pointer-events", "none"); // prevent blocking mouse events
+  const svg = tmp.append("g")
+    .attr("clip-path", "url(#clip)");
+
+  // calculate the domain based on the data points
+  const xExtent = d3.extent(dataPoints, d => d[1] * scaleLevel);
+  const yExtent = d3.extent(dataPoints, d => d[2] * scaleLevel);
+
+  // ensure the center is at (0, 0)
+  const xDomain = [Math.min(xExtent[0], -xExtent[1]), Math.max(xExtent[1], -xExtent[0])];
+  const yDomain = [Math.min(yExtent[0], -yExtent[1]), Math.max(yExtent[1], -yExtent[0])];
+
+  const xScale = d3.scaleLinear().domain(xDomain).range([margin.left, width - margin.right]).nice();
+  const yScale = d3.scaleLinear().domain(yDomain).range([height - margin.bottom, margin.top]).nice();
   
-        // append text inside the rectangle
-        tooltip.append("text")
-          .attr("x", xScale(d[1]) + 15)
-          .attr("y", yScale(d[2]) - 5)
-          .attr("text-anchor", "middle")
-          .attr("font-size", "8px")
-          .attr("fill", "#fff")
-          .text(`(${d[1].toFixed(2)}, ${d[2].toFixed(2)})`);
-      })
-      .on("mouseout", function() {
-        svg.selectAll(".popup").remove();
-      });
-    
-    // define arrowhead marker
-    svg.append("defs")
-      .append("marker")
-      .attr("id", "arrowhead")
-      .attr("viewBox", "-0 -5 10 10")
-      .attr("refX", 5)
-      .attr("refY", 0)
-      .attr("orient", "auto")
-      .attr("markerWidth", 6)
-      .attr("markerHeight", 6)
-      .append("path")
-      .attr("id", "arrowhead-path")
-      .attr("d", "M0,-5L10,0L0,5")
-      .style("fill", "red");
+  // axes
+  tmp.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(xScale));
+  tmp.append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(yScale));
 
-    // plot eigenvector arrows
-    svg.selectAll(".arrow")
-      .data(eigenvectors)
-      .enter()
-      .append("line")
-      .attr("class", "arrow")
-      .attr("x1", xScale(0))
-      .attr("y1", yScale(0))
-      .attr("x2", d => xScale(d[1] * 10))
-      .attr("y2", d => yScale(d[2] * 10))
-      .style("stroke", "red")
-      .style("stroke-width", 2)
-      .attr("marker-end", "url(#arrowhead)");
+  // add grid lines
+  svg.append("g")
+    .attr("class", "grid")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(yScale)
+      .tickSize(-width + margin.left + margin.right)
+      .tickFormat("")
+      .ticks(20)
+    );
+  
+  svg.append("g")
+    .attr("class", "grid")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(xScale)
+      .tickSize(-height + margin.top + margin.bottom)
+      .tickFormat("")
+      .ticks(20)
+    );
 
-    // add feature labels
-    svg.selectAll(".label")
-      .data(eigenvectors)
-      .enter().append("text")
-      .attr("class", "label")
-      .attr("x", d => xScale(d[1] * 10) + 10)
-      .attr("y", d => yScale(d[2] * 10) - 10)
-      .text(d => `${d[0]} (${(d[1] * 10).toFixed(2)}, ${(d[2] * 10).toFixed(2)})`)
-      .style("font-size", "8px")
-      .style("text-anchor", "middle");
-  }).catch(error => {
-    console.error("Error fetching PCA data:", error);
-    showAlert("Failed to fetch PCA data.", "danger");
-  });
+  // add title
+  tmp.append("text")
+    .attr("x", (width / 2))
+    .attr("y", margin.top / 2)
+    .attr("text-anchor", "middle")
+    .attr("class", "title")
+    .text("PCA Biplot of the Sampled Dataset");
+
+  // add axis labels
+  tmp.append("text")
+    .attr("x", width / 2)
+    .attr("y", height - margin.bottom / 4)
+    .attr("text-anchor", "middle")
+    .attr("class", "axis-label")
+    .text(components[0]);
+  
+  tmp.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", margin.left / 4)
+    .attr("text-anchor", "middle")
+    .attr("class", "axis-label")
+    .text(components[1]);
+
+  // plot data points
+  svg.selectAll(".pca-point")
+    .data(dataPoints)
+    .enter()
+    .append("circle")
+    .attr("class", "pca-point")
+    .attr("cx", d => xScale(d[1]))
+    .attr("cy", d => yScale(d[2]))
+    .attr("r", 4)
+    .style("fill", "steelblue")
+    .style("opacity", 0.6)
+    .on("mouseover", function(_, d) {
+      // append a group to hold the tooltip elements
+      let tooltip = svg.append("g")
+        .attr("class", "popup");
+
+      // append a rectangle for background
+      tooltip.append("rect")
+        .attr("x", xScale(d[1]) - 15)
+        .attr("y", yScale(d[2]) - 15)
+        .attr("width", 60)
+        .attr("height", 15)
+        .attr("fill", "#222")
+        .attr("stroke", "#ccc")
+        .attr("rx", 5) // rounded corners
+        .attr("ry", 5)
+        .style("pointer-events", "none"); // prevent blocking mouse events
+
+      // append text inside the rectangle
+      tooltip.append("text")
+        .attr("x", xScale(d[1]) + 15)
+        .attr("y", yScale(d[2]) - 5)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "8px")
+        .attr("fill", "#fff")
+        .text(`(${d[1].toFixed(2)}, ${d[2].toFixed(2)})`);
+    })
+    .on("mouseout", function() {
+      svg.selectAll(".popup").remove();
+    });
+  
+  // define arrowhead marker
+  svg.append("defs")
+    .append("marker")
+    .attr("id", "arrowhead")
+    .attr("viewBox", "-0 -5 10 10")
+    .attr("refX", 5)
+    .attr("refY", 0)
+    .attr("orient", "auto")
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .append("path")
+    .attr("id", "arrowhead-path")
+    .attr("d", "M0,-5L10,0L0,5")
+    .style("fill", "red");
+
+  // plot eigenvector arrows
+  svg.selectAll(".arrow")
+    .data(eigenvectors)
+    .enter()
+    .append("line")
+    .attr("class", "arrow")
+    .attr("x1", xScale(0))
+    .attr("y1", yScale(0))
+    .attr("x2", d => xScale(d[1] * 10))
+    .attr("y2", d => yScale(d[2] * 10))
+    .style("stroke", "red")
+    .style("stroke-width", 2)
+    .attr("marker-end", "url(#arrowhead)");
+
+  // add feature labels
+  svg.selectAll(".label")
+    .data(eigenvectors)
+    .enter().append("text")
+    .attr("class", "label")
+    .attr("x", d => xScale(d[1] * 10) + 10)
+    .attr("y", d => yScale(d[2] * 10) - 10)
+    .text(d => `${d[0]} (${(d[1] * 10).toFixed(2)}, ${(d[2] * 10).toFixed(2)})`)
+    .style("font-size", "8px")
+    .style("text-anchor", "middle");
 }
 
 // --- Initialization ---
 plotEigenvalues();
-plotPCA();
+getComponents().then((c) => plotPCA(c));
