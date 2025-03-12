12069aa33aa289285eb3aebca685790cefd95c8c
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-12 13:43:56 -0400

    add: plots map

--- lab2-b/app/static/scripts/plots.js
+++ lab2-b/app/static/scripts/plots.js
@@ -1,9 +1,32 @@
+// data MDS plot function
+function dataMDSPlot() {
+  return;
+}
+
+// variables MDS plot function
+function variablesDMSPlot() {
+  return;
+}
+
+// parallel coordinates plot function
+function pcpPlot() {
+  return;
+}
+
+// creating a map variable to reference the plot functions
+var plots = {
+  'data-mds': dataMDSPlot,
+  'variables-mds': variablesDMSPlot,
+  'pcp': pcpPlot
+};
+
 // plot function that takes a name and calls a plot function based on the name
 function plot(name) {
-    showAlert('Loading... ' + name, 'light');
+  showAlert('Drawing ' + name.toUpperCase(), 'dark');
+  plots[name]();
 }
 
 // add an event listener to the plot type dropdown to call the plot function
 document.getElementById('plot-type').addEventListener('change', function() {
-    plot(this.value)
+  plot(this.value)
 });
