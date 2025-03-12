4dc248c6438ab2320f9e9be4f42f5056ad88c110
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-11 20:52:58 -0400

    refactor: base code of lab2-b

--- lab2-b/app/static/scripts/handlers.js
+++ lab2-b/app/static/scripts/handlers.js
@@ -1,62 +1,37 @@
-/**
- * Toggle the visibility of the toolbar.
- */
-function showPopup() {
-  document.getElementById('popup').style.display = 'block';
-  document.getElementById('fade').style.display = 'block';
-}
-
-/**
- * Toggle the visibility of the toolbar.
- */
-function hidePopup() {
-  document.getElementById('popup').style.display = 'none';
-  document.getElementById('fade').style.display = 'none';
+// Toggle the visibility of the toolbar.
+function togglePopup(show) {
+  document.getElementById('popup').style.display = show ? 'block' : 'none';
+  document.getElementById('fade').style.display = show ? 'block' : 'none';
 }
 
 // --- Event Listeners ---
+// get class line and set stroke-width to the value of the range input
 document.getElementById('tickness').addEventListener('input', function() {
-  // get class line and set stroke-width to the value of the range input
   document.querySelectorAll('.line').forEach(line => line.style.strokeWidth = this.value);
   document.querySelectorAll('.arrow').forEach(line => line.style.strokeWidth = this.value);
 });
 
+// get class title and set visibility 
 document.getElementById('toggle-titles').addEventListener('change', function() {
-  if (this.checked) {
-    // get class title and set visibility to visible
-    document.querySelectorAll('.title').forEach(title => title.style.visibility = 'visible');
-  } else {
-    // get class title and set visibility to hidden
-    document.querySelectorAll('.title').forEach(title => title.style.visibility = 'hidden');
-  }
+  document.querySelectorAll('.title').forEach(title => title.style.visibility = this.checked ? 'visible' : 'hidden');
 });
 
+// get class grid and set visibility
 document.getElementById('toggle-grids').addEventListener('change', function() {
-  if (this.checked) {
-    // get class grid and set visibility to visible
-    document.querySelectorAll('.grid').forEach(grid => grid.style.visibility = 'visible');
-  } else {
-    // get class grid and set visibility to hidden
-    document.querySelectorAll('.grid').forEach(grid => grid.style.visibility = 'hidden');
-  }
+  document.querySelectorAll('.grid').forEach(grid => grid.style.visibility = this.checked ? 'visible' : 'hidden');
 });
 
+// get class axis-label and set visibility
 document.getElementById('toggle-labels').addEventListener('change', function() {
-  if (this.checked) {
-    // get class axis-label and set visibility to visible
-    document.querySelectorAll('.axis-label').forEach(label => label.style.visibility = 'visible');
-  } else {
-    // get class axis-label and set visibility to hidden
-    document.querySelectorAll('.axis-label').forEach(label => label.style.visibility = 'hidden');
-  }
+  document.querySelectorAll('.axis-label').forEach(label => label.style.visibility = this.checked ? 'visible' : 'hidden');
 });
 
+// get class line and set color to the value of the color picker
 document.getElementById('line-color-picker').addEventListener('input', function() {
-  // get class line and set color to the value of the color picker
   document.querySelectorAll('.line').forEach(line => line.style.stroke = this.value);
   document.querySelectorAll('.arrow').forEach(line => line.style.stroke = this.value);
 });
 
+// get class plot and set color to the value of the color picker
 document.getElementById('plot-color-picker').addEventListener('input', function() {
-  // get class plot and set color to the value of the color picker
 });
